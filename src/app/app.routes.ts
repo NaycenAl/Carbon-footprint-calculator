0cb203df4e569d4b3347da0aaf600f1c40a54db6
Naycen Aloulou <aloulou.naycen@gmail.com>
2024-12-10 16:53:15 +0100

    adding routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { SummaryComponent } from './components/summary/summary.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-   
+   {path : "", component: HomeComponent},
+   {path: "/summary", component : SummaryComponent }
 ];
+  
